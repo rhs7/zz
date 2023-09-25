03afa5e4798cf1aba80e86157416e0ca9c19b105
rhs7 <144723302+rhs7@users.noreply.github.com>
2023-09-25 03:15:41 +0300

    YaBh.py

---
+++ YaBh.py
@@ -0,0 +1,71 @@
+import telethon
+from telethon import events
+from telethon.sync import functions
+from telethon import TelegramClient
+import telethon
+import random
+from random import choices
+import time ;import os
+from telethon.tl.functions.messages import GetPeerDialogsRequest
+from telethon.sessions import StringSession
+from config import *
+
+auction = []
+
+band = []
+
+auction.append("vipjz\n")
+
+@StrPython.on(
+events.NewMessage(
+outgoing=True, pattern=r"حجز"))
+async def StrPychecker(event):
+        clicks = 1
+        msg = ("".join(event.text.split(maxsplit=1)[1:])).split(" ", 1)
+        
+        type = str(msg[0])
+      
+        username = await rando(type)
+        await event.reply(f"تم البدأ على النوع  - {type}")
+
+        while True:
+                clicks += 1
+                try:
+                	os.remove("clicks.txt")
+                except :
+                	open("clicks.txt","a").write(str(clicks)+"\n")
+                else:
+                	open("clicks.txt","a").write(str(clicks)+"\n")
+                try:
+             	   await StrPython(GetPeerDialogsRequest(peers=[username]))
+                except Exception as err:
+                    if "No user has" in str(err):
+                        try:
+                        	await StrPython(functions.account.UpdateUsernameRequest(username=username))           
+                        
+                        	await StrPython.send_file(event.chat_id, "https://t.me/xx_YaBh",caption=f'''
+Good evening 🤸🏻
+⌯ User ⤷ @{username}
+⌯ Save ⤷ Account
+⌯ Clicks ⤷ {clicks}
+⌯ Program the bot ⤷ @xx_YaBh''')
+                        	os.remove("clicks.txt")
+                        	break
+                        except Exception as USFL:
+                        	await StrPython.send_message(event.chat_id,f"User is error : @{username}\nError: {USLF}")
+                    else:
+                        continue                    
+                        
+                except telethon.errors.rpcerrorlist.UsernameInvalidError:
+                    	await StrPython.send_message(event.chat_id,f"User is band 🥴 : {username}")
+                    	band.append(username)StrPython.on(events.NewMessage(outgoing=True, pattern=r"ايقاف الحجز"))
+async def Shhtah(event):
+	await StrPython.send_message(event.chat_id,"جارِ ...")
+	
+	await StrPython.disconnect()
+	time.sleep(5)
+	
+	await StrPython.start()
+	await event.reply("تم")
+print("Run")
+StrPython.run_until_disconnected()
