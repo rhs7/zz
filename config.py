a084e3917bcda47e67a14abae0599a27a0d389ee
rhs7 <144723302+rhs7@users.noreply.github.com>
2024-03-29 20:43:38 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 from telethon.sync import TelegramClient
 from telethon.sessions import StringSession
 import os
-app_id = 15145595
-app_hash = "c3f60ecf742e136436acc9082ac8d9a4"
-session = os.environ.get("teleson")
+app_id = 21236276
+app_hash = "775f8c2f523b73c64a1a2149458480f2"
+session = "1BJWap1sBu1KZx1HRnnsZ_SWdF53ehdzANtsEwkeTNiaSgEn9rQJuMVALfshqtfex1WNVaA9A6AzzakKAlbPx6pWR8zHZtVW4NyqO6aSfnZtq5F0TaEngoHiYnybsX1HboIcPHNyB6hPz-nt_2UvzZSV8OLQElF5aa2BL3zjMc9lr81bKfuJj0g2mrePOKjgaOX3glNfDWEZmKqMu4Jtm_opGEerhVyeAlvt4-4CDeD-dRAtgu9ydSSJd5nN1m6cJBKDCFfU-QAelAsMVnH91ZqNnGojTqRgldTeGGMhSJFGX6ub1UbqwTIoWqeYuJHvdESguUM_ZZoG1qeINQlMOej1RKy9coU0="
 StrPython = TelegramClient(StringSession(session), app_id, app_hash)
 StrPython.start()
